8d4b7339b69cb6ad127da02072e7ff09ec545c99
rajasekhar vanjarapu <rajasekhar745@gmail.com>
2016-10-28 16:18:50 +0530

    Added about unsafe keyword

--- Csharp/Keywords/Unsafe_Pointers.cs
+++ Csharp/Keywords/Unsafe_Pointers.cs
@@ -1,1 +1,13 @@
-// https://msdn.microsoft.com/en-us/library/chfa2zb8.aspx 
+/**
+	ref : https://msdn.microsoft.com/en-us/library/chfa2zb8.aspx
+	A pointer can be any of these:
+		. pre defined data type
+		. enum
+		. user defined struct that contains fields of unmanaged types only
+		
+	Pointer types cannot be inherited from an object.
+	A pointer type cannot be converted to an object and vice versa.
+	However, we can convert between different pointer types.
+	
+	
+**/
