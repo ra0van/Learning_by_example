5c2b52b8da177833da1e44cf082cf4d10de5b7fe
rajasekhar vanjarapu <rajasekhar745@gmail.com>
2016-11-02 11:42:38 +0530

    update: unmanaged resources def & examples

--- Csharp/Keywords/Unsafe_Pointers.cs
+++ Csharp/Keywords/Unsafe_Pointers.cs
@@ -4,7 +4,12 @@
 		. pre defined data type
 		. enum
 		. user defined struct that contains fields of unmanaged types only
-		
+	What is an unmanaged type?? 
+	Unmanaged resources are anything which the garbage collector doesn't know about and anything that runs outside the CLR .For Example:
+		. Open files
+		. Open network connections
+		. SQL Connections, etc. 
+	
 	Pointer types cannot be inherited from an object.
 	A pointer type cannot be converted to an object and vice versa.
 	However, we can convert between different pointer types.
